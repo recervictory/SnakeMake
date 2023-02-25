3d69533f18551fc69b6def30e2349519a7e56223
Victor Banerjee <recervictory@gmail.com>
2023-02-25 13:58:51 +0530

    RNASeq Deseq2

--- RNAseq/deseq2.py
+++ RNAseq/deseq2.py
@@ -2,14 +2,17 @@ import dash
 from dash import Dash, html, dcc
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 # Load the DESeq2 data
-data = pd.read_csv('DESeq2_results.txt', sep='\t')
+data = pd.read_csv('results/results_airway.tsv', sep='\t')
+data['negative_padj'] = np.log10(data['padj']) * (-1)
 
 # Create the volcano plot
-fig = px.scatter(data, x='log2FoldChange', y='pvalue', 
-                 color_discrete_sequence=['red', 'gray'])
-
+fig = px.scatter(data, x='log2FoldChange', y='negative_padj', color='negative_padj',hover_name='symbol', size='negative_padj')
+fig.update_layout(
+    width=1600,
+    height=800,)
 app = Dash()
 
 app.layout = html.Div([
